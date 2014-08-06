73ff9e803664f2e33a6f6ee725592e25f13096f5
stefanheimes <stefan_heimes@hotmail.com>
2014-08-06 22:54:08 +0200

    Add the file for the http call.

---
+++ system/modules/syncCto/SyncCtoPopupFiles.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * @copyright  MEN AT WORK 2013
+ * @package    syncCto
+ * @license    GNU/LGPL
+ * @filesource
+ */
+
+/**
+ * Initialize the system
+ */
+$dir = dirname(isset($_SERVER['SCRIPT_FILENAME']) ? $_SERVER['SCRIPT_FILENAME'] : __FILE__);
+
+while ($dir && $dir != '.' && $dir != '/' && !is_file($dir . '/system/initialize.php'))
+{
+    $dir = dirname($dir);
+}
+
+if (!is_file($dir . '/system/initialize.php'))
+{
+    header("HTTP/1.0 500 Internal Server Error");
+    header('Content-Type: text/html; charset=utf-8');
+    echo '<h1>500 Internal Server Error</h1>';
+    echo '<p>Could not find initialize.php!</p>';
+    exit(1);
+}
+
+define('TL_MODE', 'BE');
+require($dir . '/system/initialize.php');
+
+/**
+ * Instantiate controller
+ */
+$objPopup = new SyncCto\Popup\Files();
+$objPopup->run();
